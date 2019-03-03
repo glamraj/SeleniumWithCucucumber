77eccb7c93b585a571a3fb64d7e5663632509e9f
RAJIB CHOWDHURY <34533128+glamraj@users.noreply.github.com>
2019-03-03 17:36:50 +0530

    Update ChromeDriver

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -24,14 +24,14 @@ public class Hook extends BaseUtil{
 
         scenarioDef = base.features.createNode(scenario.getName());
 
-        System.out.println("Opening the browser : Firefox");
+        System.out.println("Opening the browser : Chrome");  
 
         /*System.setProperty("webdriver.firefox.marionette", "D:\\Libs\\geckodriver.exe");
         base.Driver = new FirefoxDriver();*/
 
 
         //Chrome driver
-        System.setProperty("webdriver.chrome.driver", "C:\Libs\chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/opt/chromedriver");
         base.Driver = new ChromeDriver();
     }
 
